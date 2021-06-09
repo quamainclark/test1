774a79c7fcc92d798dbc9c0c2f9edf07dd1d448a
quamainclark <quamainclark1@gmail.com>
2021-06-08 22:43:03 -0400

    created app/index

--- client/src/container/App/index.js
+++ client/src/container/App/index.js
@@ -1,7 +1,37 @@
 import React from 'react';
+// import ParkingLot from '../parkingLot';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    root: {
+        width: '100%',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        background: '#9c9c5e',
+    },
+    title: {
+        color: '#151209',
+        fontSize: '3em',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 5,
+    }
+});
 
 export default function App() {
+    const classes = useStyles();
+
     return (
-        <div>Parking Lot</div>
+        <div className={classes.root}>
+            <div>
+                <div className={classes.title}>
+                    <span>Parking Lot</span>
+                </div>
+                {/* <ParkingLot /> */}
+            </div>
+        </div>
     );
 }
